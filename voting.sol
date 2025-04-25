3630665eaad2818c558f78b64e0e94db9841d1af
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-04-26 03:13:12 +0500

    coding would be tested

--- voting.sol
+++ voting.sol
@@ -4,35 +4,36 @@ pragma solidity ^0.8.22;
 contract Voting {
     struct Voter {
         string name;
-        uint age;
-        uint voterId;
+        uint256 age;
+        uint256 voterId;
         Gender gender;
-        uint voteCandidateId;
+        uint256 voteCandidateId;
         address voterAddress;
     }
 
     struct Candidate {
         string name;
         string party;
-        uint age;
+        uint256 age;
         Gender gender;
-        uint candidateId;
+        uint256 candidateId;
         address candidateAddress;
-        uint votes;
+        uint256 votes;
     }
 
     // third entity
     address electionCommission;
 
     address public winner;
-    uint nextVoterId = 1;
-    uint nextCandidateId = 1;
-    uint startTime;
-    uint endTime;
+
+    uint256 nextVoterId = 1;
+    uint256 nextCandidateId = 1;
+    uint256 startTime;
+    uint256 endTime;
     bool stopVoting;
 
-    mapping(uint => Voter) voterDetails;
-    mapping(uint => Candidate) candidateDetails;
+    mapping(uint256 => Voter) voterDetails;
+    mapping(uint256 => Candidate) candidateDetails;
 
     // representing integer values in names are "Enums"
     enum Gender {
@@ -50,10 +51,13 @@ contract Voting {
     constructor() {
         electionCommission = msg.sender;
     }
+
     modifier isVotingOver() {
         require(
-            block.timestamp <= endTime && stopVoting == false,
-            "Voting Over"
+            block.timestamp >= startTime &&
+                block.timestamp <= endTime &&
+                !stopVoting,
+            "Voting period is not active"
         );
         _;
     }
@@ -65,15 +69,18 @@ contract Voting {
     function registerCandidate(
         string calldata _name,
         string calldata _party,
-        uint _age,
+        uint256 _age,
         Gender _gender
     ) external {
-        require(_age >= 18, "You are under 18");
+        require(_age >= 18, "Age must be 18 or older");
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        require(bytes(_party).length > 0, "Party cannot be empty");
         require(
             isCandidateNotRegistered(msg.sender),
-            "Your are aleady registered"
+            "Already registered as candidate"
         );
-        require(nextCandidateId < 3, "Candidate Registration Full"); // There is election between two candidates only
+        require(nextCandidateId < 3, "Maximum candidates reached"); // There is election between two candidates only
+
         candidateDetails[nextCandidateId] = Candidate(
             _name,
             _party,
@@ -84,13 +91,18 @@ contract Voting {
             0
         );
         nextCandidateId++;
+
+        emit CandidateRegistered(msg.sender, _name);
     }
 
     function registerVoter(
         string calldata _name,
-        uint _age,
+        uint256 _age,
         Gender _gender
     ) external {
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        require(_age >= 18, "Must be 18 or older");
+        require(isVoterRegistered(msg.sender), "Already registered as voter");
         voterDetails[nextVoterId] = Voter(
             _name,
             _age,
@@ -99,14 +111,27 @@ contract Voting {
             0,
             msg.sender
         );
-        nextVoterId;
+        nextVoterId++;
+        emit VoterRegistered(msg.sender, _name);
+        // event VoterRegistered(address indexed voter, string name);
     }
 
     function isCandidateNotRegistered(
         address _person
     ) internal view returns (bool) {
-        for (uint i = 1; i < nextCandidateId; i++) {
+        for (uint256 i = 1; i < nextCandidateId; i++) {
             if (candidateDetails[i].candidateAddress == _person) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // is voter registered ?
+
+    function isVoterRegistered(address _person) internal view returns (bool) {
+        for (uint256 i = 0; i < nextVoterId; i++) {
+            if (voterDetails[i].voterAddress == _person) {
                 return false;
             }
         }
@@ -115,7 +140,7 @@ contract Voting {
 
     function getCandidateList() public view returns (Candidate[] memory) {
         Candidate[] memory candidateList = new Candidate[](nextCandidateId - 1); //initialize an empty array of length = `nextCandidateId - 1`
-        for (uint i = 0; i < candidateList.length; i++) {
+        for (uint256 i = 0; i < candidateList.length; i++) {
             candidateList[i] = candidateDetails[i + 1];
         }
         return candidateList;
@@ -123,13 +148,16 @@ contract Voting {
 
     function getVotersList() public view returns (Voter[] memory) {
         Voter[] memory voterList = new Voter[](nextVoterId - 1);
-        for (uint i = 0; i < voterList.length; i++) {
+        for (uint256 i = 1; i < voterList.length; i++) {
             voterList[i] = voterDetails[i + 1];
         }
         return voterList;
     }
 
-    function castVote(uint _voterId, uint _candidateId) public {
+    function castVote(
+        uint256 _voterId,
+        uint256 _candidateId
+    ) public isVotingOver {
         require(
             voterDetails[_voterId].voteCandidateId == 0,
             "You are already Voted"
@@ -138,6 +166,7 @@ contract Voting {
             voterDetails[_voterId].voterAddress == msg.sender,
             "Not Authorized Voter"
         );
+
         require(
             _candidateId >= 1 && _candidateId < 3,
             "Candidate Id doesn't exists"
@@ -145,15 +174,24 @@ contract Voting {
 
         voterDetails[_voterId].voteCandidateId = _candidateId;
         candidateDetails[_candidateId].votes++;
+
+        emit VoteCast(msg.sender, _candidateId);
+        // event VoteCast(address indexed voter, uint256 candidateId);
     }
 
     function setVotingPeriod(
-        uint _startTimeDuration,
-        uint _endTimeDuration
+        uint256 _startTimeDuration,
+        uint256 _endTimeDuration
     ) external onlyCommissioner {
-        require(_endTimeDuration > 3600, "must be greater then 1 hour ");
+        require(
+            _endTimeDuration >= 3600,
+            "Duration must be greater or equal to 1 hour "
+        );
         startTime = block.timestamp + _startTimeDuration;
-        endTime = _startTimeDuration + _endTimeDuration;
+        endTime = startTime + _endTimeDuration;
+
+        emit VotingPeriodSet(startTime, endTime);
+        // event VotingPeriodSet(uint256 startTime, uint256 endTime);
     }
 
     function getVotingStatus() public view returns (VotingStatus) {
@@ -167,19 +205,26 @@ contract Voting {
     }
 
     function announceVotingResult() external onlyCommissioner {
-        uint max = 0;
-        for (uint i = 1; i < nextCandidateId; i++) {
+        uint256 max = 0;
+        for (uint256 i = 1; i < nextCandidateId; i++) {
             if (candidateDetails[i].votes > max) {
                 max = candidateDetails[i].votes;
                 winner = candidateDetails[i].candidateAddress;
             }
         }
+        stopVoting = true;
     }
 
     function emergencyStopVoting() public onlyCommissioner {
         stopVoting = true;
     }
 
-    // function startVoting() {}
-    // function endVoting() {}
+    // Events for tracking activity
+    event CandidateRegistered(address indexed candidate, string name);
+    event VoterRegistered(address indexed voter, string name);
+    event VoteCast(address indexed voter, uint256 candidateId);
+    event VotingPeriodSet(uint256 startTime, uint256 endTime);
 }
+
+// start time:  1745602733 + 3600
+// end time:    1745609933
