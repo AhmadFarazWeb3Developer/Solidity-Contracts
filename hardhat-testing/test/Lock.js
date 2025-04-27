3e0d00763a93fb1cf3028f093700240c90307b6c
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-04-27 16:22:22 +0500

    Lock.sol testing is completed

--- hardhat-testing/test/Lock.js
+++ hardhat-testing/test/Lock.js
@@ -5,54 +5,66 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-describe("Lock", function () {
-  async function deployOneHourLockFixture() {
+describe("Lock", () => {
+  const deployOneHourLockFixture = async () => {
     const ONE_HOUR_IN_SEC = 3600;
-    const ONE_GWEI = 1_000_000_000;
+    const ONE_ETH = ethers.parseEther("1");
+    // one Gwei == 10^9 wei and  1 Eth = 10^18 wei
 
-    const lockedAmount = ONE_GWEI;
-
+    const lockedAmount = ONE_ETH;
     const unlockTime = (await time.latest()) + ONE_HOUR_IN_SEC;
+
+    console.log(
+      "Amount will be unloaked at : ",
+      new Date(unlockTime * 1000).toLocaleTimeString()
+    );
 
     const [owner, otherAccount] = await ethers.getSigners();
 
+    console.log("First Account ", owner.address);
+    console.log("Second Account ", otherAccount.address);
+
+    const wei = await ethers.provider.getBalance(owner);
+    console.log("Owner current balance : ", await ethers.formatEther(wei));
     const CallContract = await ethers.getContractFactory("Lock");
 
     const CONTRACT = await CallContract.deploy(unlockTime, {
       value: lockedAmount,
     });
 
-    // console.log("lock ", CONTRACT);
+    console.log("Contract hash", await CONTRACT.getAddress());
+    console.log(
+      "Contract balance : ",
+      await ethers.formatEther(await ethers.provider.getBalance(CONTRACT))
+    );
 
     return { CONTRACT, unlockTime, lockedAmount, owner, otherAccount };
-  }
+  };
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { CONTRACT, unlockTime } = await loadFixture(
+  describe("Deployment", () => {
+    //  run setup before each it.
+    beforeEach(async () => {
+      ({ CONTRACT, owner, unlockTime, lockedAmount } = await loadFixture(
         deployOneHourLockFixture
-      );
+      ));
+    });
 
+    // run actual test case
+    it("Should set the right unlockTime", async () => {
       expect(await CONTRACT.unlockTime()).to.equal(unlockTime);
     });
 
-    it("Should set the right owner", async function () {
-      const { CONTRACT, owner } = await loadFixture(deployOneHourLockFixture);
-
+    it("Should set the right owner", async () => {
       expect(await CONTRACT.owner()).to.equal(owner.address);
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { CONTRACT, lockedAmount } = await loadFixture(
-        deployOneHourLockFixture
-      );
-
+    it("Should receive and store the funds to lock", async () => {
       expect(await ethers.provider.getBalance(CONTRACT.target)).to.equal(
         lockedAmount
       );
     });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
+    it("Should fail if the unlockTime is not in the future", async () => {
       const latestTime = await time.latest();
 
       const Lock = await ethers.getContractFactory("Lock");
@@ -63,9 +75,9 @@ describe("Lock", function () {
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
+  describe("Withdrawals", () => {
+    describe("Validations", () => {
+      it("Should revert with the right error if called too soon", async () => {
         const { CONTRACT } = await loadFixture(deployOneHourLockFixture);
 
         await expect(CONTRACT.withdraw()).to.be.revertedWith(
@@ -73,7 +85,7 @@ describe("Lock", function () {
         );
       });
 
-      it("Should revert with the right error if called from another account", async function () {
+      it("Should revert with the right error if called from another account", async () => {
         const { CONTRACT, unlockTime, otherAccount } = await loadFixture(
           deployOneHourLockFixture
         );
@@ -87,7 +99,7 @@ describe("Lock", function () {
         ).to.be.revertedWith("You aren't the owner");
       });
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async () => {
         const { CONTRACT, unlockTime } = await loadFixture(
           deployOneHourLockFixture
         );
@@ -99,8 +111,8 @@ describe("Lock", function () {
       });
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
+    describe("Events", () => {
+      it("Should emit an event on withdrawals", async () => {
         const { CONTRACT, unlockTime, lockedAmount } = await loadFixture(
           deployOneHourLockFixture
         );
@@ -113,8 +125,8 @@ describe("Lock", function () {
       });
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
+    describe("Transfers", () => {
+      it("Should transfer the funds to the owner", async () => {
         const { CONTRACT, unlockTime, lockedAmount, owner } = await loadFixture(
           deployOneHourLockFixture
         );
@@ -125,6 +137,9 @@ describe("Lock", function () {
           [owner, CONTRACT],
           [lockedAmount, -lockedAmount]
         );
+
+        const wei = await ethers.provider.getBalance(owner);
+        console.log("Owner new balance : ", await ethers.formatEther(wei));
       });
     });
   });
