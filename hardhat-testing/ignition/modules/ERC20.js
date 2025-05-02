d58a0f457377c05fb513416e5e2c038e6c372acc
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-05-02 19:34:18 +0500

    All three contract erc20, tokenMarketplace and voting contract deployed to hardhat local network

--- hardhat-testing/ignition/modules/ERC20.js
+++ hardhat-testing/ignition/modules/ERC20.js
@@ -1,9 +1,13 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const pkbk_deployedContract = buildModule("PKBK_module", (m) => {
+const MainDeployedContract = buildModule("PKBK_module", (m) => {
   const initalSupply = m.getParameter("initialSupply", 1000n * 10n ** 18n);
-  const pkbk = m.contract("PKBK", [initalSupply]);
-  return { pkbk };
+  const pkbk_contract = m.contract("PKBK", [initalSupply]);
+  const tokenMarketPlace_contract = m.contract("TokenMarketPlace", [
+    pkbk_contract,
+  ]);
+  const voting_contract = m.contract("Voting", [pkbk_contract]);
+  return { pkbk_contract, tokenMarketPlace_contract, voting_contract };
 });
 
-module.exports = pkbk_deployedContract;
+module.exports = MainDeployedContract;
