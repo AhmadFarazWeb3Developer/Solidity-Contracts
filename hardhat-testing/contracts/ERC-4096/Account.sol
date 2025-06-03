5cab52fca7c7b5b3379af810c6e0dd97c095cab0
AhmadFarazWeb3Developer <Ahmadfaraz.web3.0.developer@gmail.com>
2025-06-03 12:47:57 +0500

    creating smart account using Account Abstraction

--- hardhat-testing/contracts/ERC-4096/Account.sol
+++ hardhat-testing/contracts/ERC-4096/Account.sol
@@ -2,3 +2,56 @@
 pragma solidity ^0.8.22;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+// import "@account-abstraction/contracts/interfaces/UserOperation.sol";
+
+contract Account is IAccount {
+    uint256 public count;
+    address public owner;
+    IEntryPoint public immutable entryPoint;
+
+    constructor(address _owner, IEntryPoint _entryPoint) {
+        owner = _owner;
+        entryPoint = _entryPoint;
+    }
+
+    /// @notice Called by EntryPoint to validate the user operation
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 /* missingAccountFunds */
+    ) external view override returns (uint256 validationData) {
+        require(msg.sender == address(entryPoint), "Only EntryPoint can call");
+
+        // Recreate Ethereum signed message hash
+        bytes32 ethSignedHash = ECDSA.toEthSignedMessageHash(userOpHash);
+
+        // Recover signer
+        address recovered = ECDSA.recover(ethSignedHash, userOp.signature);
+
+        // Validate
+        if (recovered != owner) {
+            return 1; // invalid
+        }
+
+        return 0; // valid
+    }
+
+    /// @notice Only EntryPoint can call actual execution
+    function execute() external {
+        require(msg.sender == address(entryPoint), "Only EntryPoint can call");
+        count++;
+    }
+}
+
+contract AccountFactory {
+    function createAccount(
+        address owner,
+        IEntryPoint entryPoint
+    ) external returns (address) {
+        Account account = new Account(owner, entryPoint);
+        return address(account);
+    }
+}
