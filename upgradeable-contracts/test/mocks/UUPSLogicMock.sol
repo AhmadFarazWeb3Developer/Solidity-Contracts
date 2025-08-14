3ebd46af29bff7ee80cf0c68e8089e002b71689b
AhmadFarazWeb3Developer <ahmadfaraz.web3.0.developer@gmail.com>
2025-08-14 13:59:35 +0500

    testing UUPS

--- upgradeable-contracts/test/mocks/UUPSLogicMock.sol
+++ upgradeable-contracts/test/mocks/UUPSLogicMock.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.25;
 
 import {UUPSUpgradeable} from "../../src/UUPS_Upgradeable/UUPSUpgradeable.sol";
+import {Initializable} from "../../src/UPS_Upgradeable/Initializable.sol";
 
-contract UUPSLogicMock is UUPSUpgradeable {
+contract UUPSLogicMock is UUPSUpgradeable, Initializable {
+
     uint256 public value;
+
+
+
+
+
 
     function Initializer(uint256 _value) public {
         value = _value;
