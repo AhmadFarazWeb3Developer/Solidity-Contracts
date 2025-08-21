0f1a5900c3241b128fc37c321ff1533f1b5af963
AhmadFarazWeb3Developer <ahmadfaraz.web3.0.developer@gmail.com>
2025-08-21 23:50:31 +0500

    tested

--- EVM/src/StorageVariables.sol
+++ EVM/src/StorageVariables.sol
@@ -11,28 +11,42 @@ contract StorageVariables {
     uint256 constant b = 10; // goes to contract byte code direcly not slots.
     uint8 c = 5; // storage slot 0x1 (packed into the same slot-2)
     uint16 d = 10; // storage slot 0x1 (packed into the same slot-2)
-
-    bool e; // defaults to false, HEX value would be 0x....0000 (64 characters)
-    uint f; // storage slot 0x3
+    bool e; // defaults to false, take 1 byte, storage slot 0x1 (packed inot same slot-2)
+    uint f; // default to 0, takes 32 bytes , storage slot 0x3,
 
     constructor() {
+        // hex values at slots
         bytes32 slot1Val;
         bytes32 slot2Val;
         bytes32 slot3Val;
-        bytes32 slot4Val;
-        bytes32 slot5Val;
+
+        uint256 x_slot;
+        uint256 c_slot;
+        uint256 d_slot;
+        uint256 e_slot;
+        uint256 f_slot;
+
         assembly {
+            // Yul
+
+            x_slot := a.slot
+            c_slot := c.slot
+            d_slot := d.slot
+            e_slot := e.slot
+            f_slot := f.slot
+
             slot1Val := sload(0x0)
             slot2Val := sload(0x1)
             slot3Val := sload(0x2)
-            slot4Val := sload(0x3)
-            slot5Val := sload(0x4)
         }
 
         console.logBytes32(slot1Val);
         console.logBytes32(slot2Val);
         console.logBytes32(slot3Val);
-        console.logBytes32(slot4Val);
-        console.logBytes32(slot5Val);
+        console.log(x_slot);
+        console.log(c_slot);
+        console.log(d_slot);
+        console.log(e_slot);
+        console.log(f_slot);
     }
 }
