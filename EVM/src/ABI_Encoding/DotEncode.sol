4fad47fd5fbfdd71755c69e1a105832d4f882f38
AhmadFarazWeb3Developer <ahmadfaraz.web3.0.developer@gmail.com>
2025-08-23 17:23:19 +0500

    struct is encoded

--- EVM/src/ABI_Encoding/DotEncode.sol
+++ EVM/src/ABI_Encoding/DotEncode.sol
@@ -24,11 +24,24 @@ contract DotEncode {
 
     //   Fixed-size byte values (like bytes4, bytes8, bytes12, etc.) are zero-padded on the right side.
     function BytesEncode() external pure returns (bytes memory) {
-        return abi.encode(0xabcdef12);
+        // return abi.encode(0xabcdef12); // this is not hex .
+        /*
+        uint256 can store:
+        Decimal: 123456789
+        Hexadecimal: 0xabcdef12
+        Binary: 0b10101010
+        Octal: 0o1234567
+         */
+
+        return abi.encode(bytes4(0xabcdef12)); // fixed size bytes, no extra slot for offset and length required
     }
 
     function UintsEncode() external pure returns (bytes memory) {
         return abi.encode(213451213231231231231231231231231235);
+    }
+
+    function BooleanEncode() external pure returns (bytes memory) {
+        return abi.encode(true);
     }
 
     function StaticMix() external pure returns (bytes memory) {
@@ -36,19 +49,67 @@ contract DotEncode {
             abi.encode(
                 0xD6b0794b7aeA7190F1831A40393e599CC0864EB3,
                 121312,
-                0xabcdef12
+                bytes4(0xabcdef12)
             );
     }
-    // ENCODING OF DYNAMIC TYPES
+
+    // -------------------- ENCODING OF DYNAMIC TYPES ------------------------------------------------
 
     function StringEncode() external pure returns (bytes memory) {
         return abi.encode("YUL");
+        /*
+    Memory is continous bytes:
+
+    Byte 0-31:   0x0000...0020 (offset)
+    Byte 32-63:  0x0000...0003 (length) 
+    Byte 64-95:  0x59554c...00 (data)
+    */
+    }
+
+    // when we write bytes, it become dynamic it will have require offset, length slots extra because we dont know the size
+    // of data at runtime
+    function DynamicBytesEncode(
+        bytes memory _data
+    ) external pure returns (bytes memory) {
+        return (abi.encode(_data));
+    }
+
+    function UintsArrayEncode() external pure returns (bytes memory) {
+        uint256[] memory uintArrays = new uint256[](2);
+        uintArrays[0] = 143;
+        uintArrays[1] = 100;
+        return abi.encode(uintArrays);
+    }
+
+    function BytesArrayEncode() external pure returns (bytes memory) {
+        bytes4[] memory bytesArray = new bytes4[](2);
+        bytesArray[0] = bytes4(0x123456e1);
+        bytesArray[1] = bytes4(0xa12e3f8f);
+        return abi.encode(bytesArray);
+    }
+
+    function StringArrayEncode() external pure returns (bytes memory) {
+        string[] memory stringArray = new string[](2);
+        stringArray[0] = "Hello are you enjoying the YUL Programming language";
+        stringArray[1] = "Yes solidity made it awesome";
+
+        return abi.encode(stringArray);
+    }
+
+    struct myStruct {
+        string data;
+        uint256 id;
+    }
+
+    function StructEncode() external pure returns (bytes memory) {
+        myStruct memory s = myStruct({id: 1122, data: "Hello Yul!"});
+        return abi.encode(s);
     }
 }
 
 /*
-    The offset position doesn't change based on the string's position in the function parameters!
+    The offset position doesn't change based on thse string's position in the function parameters!
     Key rule: In abi.encode, all static types come first, then all dynamic types, regardless of parameter order.
-     */
+    */
 
 // contract.call(data) ?
--- EVM/test/DotEncodet.t.sol
+++ EVM/test/DotEncodet.t.sol
@@ -12,25 +12,191 @@ contract DotEncodeTest is UtilsTest {
     function test_AddressEncode() public view {
         dotEncode.AddressEncode(); // 0x000000000000000000000000d6b0794b7aea7190f1831a40393e599cc0864eb3
     }
+
     function test_BytesEncode() public view {
         dotEncode.BytesEncode(); // 0x00000000000000000000000000000000000000000000000000000000abcdef12
     }
+
     function test_UintsEncode() public view {
-        dotEncode.UintsEncode(); //  0x0000000000000000000000000000000000291bf5139ecbf97aeba956631f8503
+        dotEncode.UintsEncode(); // 0x0000000000000000000000000000000000291bf5139ecbf97aeba956631f8503
+    }
+
+    function test_BooleanEncode() public view {
+        dotEncode.BooleanEncode(); // 0x0000000000000000000000000000000000000000000000000000000000000001
     }
 
     function test_StaticMix() public view {
-        dotEncode.StaticMix(); // 0x000000000000000000000000d6b0794b7aea7190f1831a40393e599cc0864eb3000000000000000000000000000000000000000000000000000000000001d9e000000000000000000000000000000000000000000000000000000000abcdef12
+        bytes memory data = dotEncode.StaticMix(); // 0x000000000000000000000000d6b0794b7aea7190f1831a40393e599cc0864eb3000000000000000000000000000000000000000000000000000000000001d9e0abcdef1200000000000000000000000000000000000000000000000000000000
+
+        (address ADDRESS, uint256 VAR, bytes4 BYT) = abi.decode(
+            data,
+            (address, uint256, bytes4)
+        );
+
+        console.log(ADDRESS);
+        console.log(VAR);
+        console.logBytes4(BYT);
     }
 
     // Dynamic Types Encoding
-
     function test_StringEncode() public view {
         dotEncode.StringEncode();
         /*
+            0x0000000000000000000000000000000000000000000000000000000000000020 -> offset
+            0000000000000000000000000000000000000000000000000000000000000003 -> length
+            59554c0000000000000000000000000000000000000000000000000000000000 -> content
+        */
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    //----------------------- DYNAMIC TYPES ENCODING --------------------------------------------
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    function test_DynamicBytesEncode() public view {
+        // READ : dotEncode.DynamicBytesEncode(bytes(0x1212fdaffcde5));
+        // Cannot do this. The issue is that 0x1212fdaffcde5 is a number literal, and you can't directly cast numbers to bytes memory.
+
+        // Casting to Bytes
+
+        // 1 method:
+        dotEncode.DynamicBytesEncode(hex"01212fdaffcde5"); // add 0 to start for hex understanding
+
+        /*
+            DynamicBytesEncode(0x01212fdaffcde5) 
+                [Return] 0x0000000000000000000000000000000000000000000000000000000000000020
+                         0000000000000000000000000000000000000000000000000000000000000007
+                         01212fdaffcde500000000000000000000000000000000000000000000000000
+        */
+
+        // 2 method:
+        dotEncode.DynamicBytesEncode(
+            abi.encodePacked(uint256(0x1212fdaffcde5))
+        );
+
+        /*
+            DynamicBytesEncode(0x0000000000000000000000000000000000000000000000000001212fdaffcde5) 
+                [Return] 0x0000000000000000000000000000000000000000000000000000000000000020
+                         0000000000000000000000000000000000000000000000000000000000000020
+                         0000000000000000000000000000000000000000000000000001212fdaffcde5
+        */
+
+        // 3 method:
+        dotEncode.DynamicBytesEncode("\x12\x12\xfd\xaf\xfc\xde\x05");
+
+        /*
+            DynamicBytesEncode(0x1212fdaffcde05) [staticcall]
+                [Return] 0x0000000000000000000000000000000000000000000000000000000000000020
+                         0000000000000000000000000000000000000000000000000000000000000007
+                         1212fdaffcde0500000000000000000000000000000000000000000000000000
+        */
+    }
+
+    function test_UintsArrayEncode() public view {
+        bytes memory hashedData = dotEncode.UintsArrayEncode();
+        /*
         0x0000000000000000000000000000000000000000000000000000000000000020 -> offset
-          0000000000000000000000000000000000000000000000000000000000000003 -> length
-          59554c0000000000000000000000000000000000000000000000000000000000 -> content
+          0000000000000000000000000000000000000000000000000000000000000001 -> length
+          000000000000000000000000000000000000000000000000000000000000008f -> first element
+          000000000000000000000000000000000000000000000000000000000000064  -> second element            
+         */
+
+        // DECODING
+        uint256[] memory uintArray = abi.decode(hashedData, (uint256[]));
+        for (uint256 i = 0; i < uintArray.length; i++) {
+            console.log(uintArray[i]);
+        }
+    }
+    function test_BytesArrayEncode() public view {
+        bytes memory hashedData = dotEncode.BytesArrayEncode();
+        /*
+         0x0000000000000000000000000000000000000000000000000000000000000020 -> offset
+           0000000000000000000000000000000000000000000000000000000000000002 -> length
+           123456e100000000000000000000000000000000000000000000000000000000 -> first bytes element
+           a12e3f8f00000000000000000000000000000000000000000000000000000000 -> seconf bytes element      
+         */
+
+        // DECODING
+        bytes4[] memory bytesArray = abi.decode(hashedData, (bytes4[]));
+        for (uint256 i = 0; i < bytesArray.length; i++) {
+            console.logBytes4(bytesArray[i]);
+        }
+    }
+
+    function test_StringArrayEncode() public view {
+        bytes memory hashedData = dotEncode.StringArrayEncode();
+        /*
+
+        WHEN :
+        stringArray = ["Hello are you enjoying the YUL", "Yes solidity made it awesome"]
+
+        0x0000000000000000000000000000000000000000000000000000000000000020 -> offset of array 
+         0000000000000000000000000000000000000000000000000000000000000002 -> lenght of array
+         0000000000000000000000000000000000000000000000000000000000000040 -> first index offset
+         0000000000000000000000000000000000000000000000000000000000000080 -> second index offset
+         000000000000000000000000000000000000000000000000000000000000001d -> length of first element
+         48656c6c6f20617220796f7520656e6a6f79696e67207468652059554c000000 -> first index content 
+         000000000000000000000000000000000000000000000000000000000000001c -> length of second element 
+         59657320736f6c6964697479206d61646520697420617765736f6d6500000000 -> second index content
+
+
+
+        WHEN : 
+        stringArray = ["Hello are you enjoying the YUL Programming language", "Yes solidity made it awesome"]
+
+        0x0000000000000000000000000000000000000000000000000000000000000020 -> offset of array 
+         0000000000000000000000000000000000000000000000000000000000000002 -> length of array
+         0000000000000000000000000000000000000000000000000000000000000040 -> first index offset
+         00000000000000000000000000000000000000000000000000000000000000a0 -> second index offset
+         0000000000000000000000000000000000000000000000000000000000000033 -> lenght of first index
+         48656c6c6f2061726520796f7520656e6a6f79696e67207468652059554c2050 -> first index content
+         726f6772616d6d696e67206c616e677561676500000000000000000000000000 
+         000000000000000000000000000000000000000000000000000000000000001c -> length of second index
+         59657320736f6c6964697479206d61646520697420617765736f6d6500000000 -> second index content
+        */
+        // DECODING
+        string[] memory stringArray = abi.decode(hashedData, (string[]));
+        for (uint256 i = 0; i < stringArray.length; i++) {
+            console.log(stringArray[i]);
+        }
+    }
+
+    function test_StructEncode() public view {
+        dotEncode.StructEncode();
+
+        /*
+        struct myStruct {
+            string data;
+            uint256 id;
+        }
+          myStruct memory s = myStruct({id: 1122, data: "Hello Yul!"});
+
+          0x0000000000000000000000000000000000000000000000000000000000000020 -> offset
+            0000000000000000000000000000000000000000000000000000000000000462 -> 1122
+            0000000000000000000000000000000000000000000000000000000000000040 -> string offset
+            000000000000000000000000000000000000000000000000000000000000000a -> length of string 
+            48656c6c6f2059756c2100000000000000000000000000000000000000000000 -> content
+        
+
+
+
+
+        struct myStruct {
+            string data;
+            uint256 id;
+        }
+
+        0x0000000000000000000000000000000000000000000000000000000000000020 -> offset
+          0000000000000000000000000000000000000000000000000000000000000040 -> string offset 
+          0000000000000000000000000000000000000000000000000000000000000462 -> 1122
+          000000000000000000000000000000000000000000000000000000000000000a -> length of string
+          48656c6c6f2059756c2100000000000000000000000000000000000000000000 -> content
+
+
+        Solidity does not care about the order of types ,
+        if the types if dyamic or refrecen type it hold the pointer to it
+
          */
     }
 }
@@ -47,7 +213,6 @@ Some Solidity types are not supported by the ABI, but they are able to be repres
   │ user defined value types  │ underlying value type     │
   │ struct                    │ tuple                     │
   └───────────────────────────┴───────────────────────────┘
-
 
   It means if we try to abi.encode(payable(anyAddress)), so payable does not effect the encoded bytes 
 
